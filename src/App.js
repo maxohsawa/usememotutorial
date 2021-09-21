66f24937b94d73b512c4636b5913c8b96eacb8cc
maxohsawa <max.ohsawa@gmail.com>
2021-09-21 09:37:18 -0400

    complete usememo tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useEffect } from 'react';
 
-function App() {
+export default function App() {
+
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+
+  const doubleNumber =  useMemo( () => {
+    return slowFunction(number) 
+  }, [number]);
+
+  const themeStyles = useMemo( () => {
+    return {
+      backgroundColor: dark ? 'black' : 'white',
+      color: dark ? 'white' : 'black'
+    }
+  }, [dark]);
+
+  useEffect( () => {
+    console.log('Theme changed');
+  }, [themeStyles]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="number" value={number} onChange={ e => setNumber(parseInt(e.target.value))} />
+      <button onClick={ () => setDark(prevDark => !prevDark)}>Change Theme </button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </div>
   );
 }
 
-export default App;
+function slowFunction(num) {
+  console.log('Calling slow function');
+  for(let i = 0; i < 1000000000; i++){}
+  return num * 2;
+}
